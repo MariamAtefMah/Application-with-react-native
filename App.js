77a37613726da155c2c5cfc0cdde0cc38c5a4e0c
MariamAtefMah <meroatef451@gmail.com>
2023-10-30 12:48:35 +0200

    Making first App and Button

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
+import React, {useState} from 'react'; // we can write: import { StatusBar } from 'expo-status-bar';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native'; //Button is self closing element 
 
 export default function App() {
+  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!')
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{outputText}</Text>
+      <Button title='change text'  onPress={() => setOutputText('Happy beginning')}/>  
+      <StatusBar style="auto"/>
     </View>
   );
 }
@@ -13,7 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#679b9b',
     alignItems: 'center',
     justifyContent: 'center',
   },
